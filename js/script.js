a6ef78bb7b83d05b1fe811b521e5354b3faec95f
nika19du <qnkovanikol29@gmail.com>
2020-11-05 15:33:26 +0200

    added next &prv btn; showing videos

--- js/script.js
+++ js/script.js
@@ -75,32 +75,111 @@ function getOutput(item){
  var thumb=item.snippet.thumbnails.high.url;
  var channelTitle=item.snippet.channelTitle;
  var videoDate=item.snippet.publishedAt;
-
- //Build Output String
+//Build Output String
  var output='<li>'+
- '<div class="list-left">'+
- '<img src="'+thumb+'">'+
- '</div>'+
- '<div class="list-right">'+
- '<h3>'+title+'</h3>'+
- '<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>'+
- '<p>'+description+'</p>'+
- '</div>'+
- '</li>'+
- '<div class="clearflix"></div>'+
- '';
- return output;
+'<div class="list-left">'+
+'<img src="'+thumb+'">'+
+'</div>'+
+'<div class="list-right">'+
+'<h3><a class="fancybox fancybox.iframe" href="https://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>'+
+'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>'+
+'<p>'+description+'</p>'+
+'</div>'+
+'</li>'+
+'<div class="clearflix"></div>'+
+'';
+return output;
 }
+
+ //Next Page Functions
+ function nextPage(){
+  var token=$('#next-button').data('token');
+  var q=$('#next-button').data('query');
+   //Clear Results
+ $('#results').html('');
+ $('#buttons').html(''); 
+ //Get Form Input
+ q=$('#query').val(); 
+ //Get Request on API
+ $.get(
+  "https://www.googleapis.com/youtube/v3/search",{
+   part:'snippet, id',
+   q:q,
+   pageToken:token,
+   type:'video',
+   key:'AIzaSyDxyaPDYnrPDF-SY1Et791JIGuJWBcLMUU'},
+   function(data){
+    var nextPageToken=data.nextPageToken;
+    var prevPageToken=data.prevPageToken;
+    //Log Data
+    console.log(data);
+
+    $.each(data.items,function(i,item){
+     //Get Output
+     var output=getOutput(item);
+
+     //Display Results
+     $('#results').append(output);
+    });
+
+    var buttons=getButtons(prevPageToken,nextPageToken);
+
+    //Display Buttons
+    $('#buttons').append(buttons);
+   }
+ );
+ }
+
+//Prev Page Functions
+function prevPage(){
+  var token=$('#prev-button').data('token');
+  var q=$('#prev-button').data('query');
+   //Clear Results
+  $('#results').html('');
+  $('#buttons').html(''); 
+ //Get Form Input
+  q=$('#query').val(); 
+ //Get Request on API
+  $.get(
+  "https://www.googleapis.com/youtube/v3/search",{
+  part:'snippet, id',
+  q:q,
+  pageToken:token,
+  type:'video',
+  key:'AIzaSyDxyaPDYnrPDF-SY1Et791JIGuJWBcLMUU'
+  },
+  function(data){
+    var nextPageToken=data.nextPageToken;
+    var prevPageToken=data.prevPageToken;
+    //Log Data
+    console.log(data);
+
+    $.each(data.items,function(i,item){
+     //Get Output
+    var output=getOutput(item);
+
+     //Display Results
+    $('#results').append(output);
+    });
+
+    var buttons=getButtons(prevPageToken,nextPageToken);
+
+    //Display Buttons
+    $('#buttons').append(buttons);
+}
+);
+}
+
 
 //Build the Buttons
 function getButtons(prevPageToken,nextPageToken){
- if(!prevPageToken){
+  if(!prevPageToken){
   var btnoutput='<div class="button-container">'+
-  '<button id="next-button" class="pagging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'+'onclick="nextPage();">Next Page</button></div>';
- }else{
+  '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'+'onclick="nextPage();">Next Page</button></div>';
+  }else{
   var btnoutput='<div class="button-container">'+
-  '<button id="next-button" class="pagging-button" data-token="'+prevPageToken+'" data-query="'+q+'"'+'onclick="prevPage();">Prev Page</button>'+
-  '<button id="next-button" class="pagging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'+'onclick="nextPage();">Next Page</button></div>';
- }
- return btnoutput;
+  '<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"'+'onclick="prevPage();">Prev Page</button>'+
+  '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'+'onclick="nextPage();">Next Page</button></div>';
+  }
+  return btnoutput;
 }
